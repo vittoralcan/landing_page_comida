367ce2c4744a1de18b6a2e45d0b0493dfa7694bc
João Vítor O. de Alcantara <joaovitoredt@hotmail.com>
2024-12-25 23:08:53 -0300

    Atualizações do projeto

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll',function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight ();
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+        }
+        
+    })
 });
+
