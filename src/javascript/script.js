1eb4d691f9d89b2bf72141ad6015cb5861eedf45
João Vítor O. de Alcantara <joaovitoredt@hotmail.com>
2024-12-26 18:27:24 -0300

    Atualizações finais do projeto

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -33,5 +33,28 @@ $(document).ready(function() {
         navItems.removeClass('active');
         $(navItems[activeSectionIndex]).addClass('active');
     });
-});
 
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+});
